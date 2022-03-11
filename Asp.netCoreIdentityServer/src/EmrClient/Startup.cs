3db776bdbe0b02e0fabc3ddc244751be2df6060c
bereket fiseha <bereketfiseha123@gmail.com>
2022-03-11 16:52:14 +0300

    Oracle Identity Server Login Page Update

--- Asp.netCoreIdentityServer/src/EmrClient/Startup.cs
+++ Asp.netCoreIdentityServer/src/EmrClient/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -49,6 +50,9 @@ services.AddAuthentication(options =>
         options.Scope.Add("emrapi");
         options.Scope.Add("offline_access");
     options.GetClaimsFromUserInfoEndpoint = true;
+        options.ClaimActions.MapJsonKey("role", "role", "role");
+        options.TokenValidationParameters.NameClaimType = "name";
+        options.TokenValidationParameters.RoleClaimType = "role";
     });
         }
 
--- Asp.netCoreIdentityServer/src/IdentityServerAspNetIdentity/Quickstart/SecurityHeadersAttribute.cs
+++ Asp.netCoreIdentityServer/src/IdentityServerAspNetIdentity/Quickstart/SecurityHeadersAttribute.cs
@@ -36,7 +36,7 @@ namespace IdentityServerHost.Quickstart.UI
                 // once for standards compliant browsers
                 if (!context.HttpContext.Response.Headers.ContainsKey("Content-Security-Policy"))
                 {
-                    context.HttpContext.Response.Headers.Add("Content-Security-Policy", csp);
+                 //   context.HttpContext.Response.Headers.Add("Content-Security-Policy", csp);
                 }
                 // and once again for IE
                 if (!context.HttpContext.Response.Headers.ContainsKey("X-Content-Security-Policy"))
